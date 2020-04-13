fd01ef0bcea0b808ed8c244ff658da915da6d120
aeksei <aeksei94@gmail.com>
2020-04-13 12:32:29 +0300

    init

---
+++ template-master/src/App.svelte
@@ -0,0 +1,75 @@
+<script>
+
+    import { onMount, onDestroy } from 'svelte';
+    import Cookies from 'js-cookie/src/js.cookie.js';
+
+   let food = [];
+
+    const CSRF_TOKEN = Cookies.get('csrftoken');
+    const WISHLIST_URL = getRef("wishlist-ref");  // http://localhost:8000/wishlist
+
+    function getRef(id) {return document.getElementById(id).href;};
+
+    onMount(async () => {
+       const response = await fetch(WISHLIST_URL, {
+                                     headers: {
+                                        'Accept': 'application/json, text-plain, */*',
+                                        'X-Requested-With': 'XMLHttpRequest',
+                                     }, });
+
+       let food_json = await response.json();
+        // food_json = food_json;
+        food = food_json['food']
+        food = food
+        console.log(food);
+       console.log(typeof(food));
+   });
+
+</script>
+
+<main>
+    
+<table class="table">
+    <thead class="thead-primary">
+      <tr class="text-center">
+        <th>&nbsp;</th>
+        <th>Product List</th>
+        <th>&nbsp;</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Total</th>
+      </tr>
+    </thead>
+    <tbody >
+        {#each food as item }
+          <tr class="text-center">
+            <td class="product-remove"><a href="."><span class="ion-ios-close"></span></a></td>
+            <td class="image-prod"><div class="img" style="background-image:{'url(/static/shop/' + item.img + ')'};"></div></td>
+            <td class="product-name">
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+            </td>						        
+            <td class="price">{item.currency}{item.price}</td>						        
+            <td class="quantity">
+                <div class="input-group mb-3">
+                <input type="number" name="quantity" class="quantity form-control input-number" bind:value={item.quantity} min=1 max=100>
+            </div>
+          </td>						        
+            <td class="total">{item.currency}{item.quantity*item.price}</td>
+          </tr><!-- END TR-->
+        {/each}
+
+    </tbody>
+  </table>
+
+
+</main>
+
+<style>
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ vshop/shop/views.py
@@ -0,0 +1,156 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import View
+from .settings.base import *
+from django.core.paginator import Paginator
+
+from .models import *
+from django.http import HttpResponse
+import json
+# Create your views here.
+
+class IndexView(View):
+
+    def get(self, request):
+    
+        for k, v in request.COOKIES.items():
+            print(k, v)
+    
+        return render(request, 'shop/index.html', {'phone_number': PHONE_NUMBER})
+
+
+class ShopView(View):
+
+    def get(self, request, page_id=1):
+        products_list = [{'name':       'Bell Pepper',
+                     'image':      'shop/images/product-1.jpg',
+                     'price':      '$120.00',
+                     'discount':   '30%',
+                     'price_sale': '$80.00'}, 
+                    {'name':       'Strawberry',
+                     'image':      'shop/images/product-2.jpg',
+                     'price':      '$120.00'}, 
+                    {'name':       'Green Beans',
+                     'image':      'shop/images/product-3.jpg',
+                     'price':      '$120.00'}, 
+                    {'name':       'Purple Cabbage',
+                     'image':      'shop/images/product-4.jpg',
+                     'price':      '$120.00'},
+                    {'name':       'Tomatoe',
+                     'image':      'shop/images/product-5.jpg',
+                     'price':      '$120.00',
+                     'discount':   '30%',
+                     'price_sale': '$80.00'},   
+                    {'name':       'Brocolli',
+                     'image':      'shop/images/product-6.jpg',
+                     'price':      '$120.00'}, 
+                    {'name':       'Carrots',
+                     'image':      'shop/images/product-7.jpg',
+                     'price':      '$120.00'}, 
+                    {'name':       'Fruit Juice',
+                     'image':      'shop/images/product-8.jpg',
+                     'price':      '$120.00'},     
+                    {'name':       'Onion',
+                     'image':      'shop/images/product-9.jpg',
+                     'price':      '$120.00',
+                     'discount':   '30%',
+                     'price_sale': '$80.00'},    
+                    {'name':       'Apple',
+                     'image':      'shop/images/product-10.jpg',
+                     'price':      '$120.00'},   
+                    {'name':       'Garlic',
+                     'image':      'shop/images/product-11.jpg',
+                     'price':      '$120.00'},     
+                    {'name':       'Chilli',
+                     'image':      'shop/images/product-12.jpg',
+                     'price':      '$120.00'}]
+
+        PRODUCTS_ON_PAGE = 4
+        count_page = len(products_list) // 4
+        if len(products_list) % PRODUCTS_ON_PAGE:
+            count_page += 1
+                   
+        paginator = Paginator(products_list, PRODUCTS_ON_PAGE)
+        
+        try:
+            products = paginator.page(page_id)
+            products.num_pages_tuple = tuple(range(paginator.num_pages))
+        except:
+            return redirect(reverse('shop'))
+
+        if not request.is_ajax():
+            return render(request, 'shop/shop.html', {'products': products, 'phone_number': PHONE_NUMBER})
+        else:
+            products = products_list[(page_id - 1) * PRODUCTS_ON_PAGE: page_id * PRODUCTS_ON_PAGE]
+            return HttpResponse(json.dumps({'products': products,
+                                            'count_page': count_page}),
+                                content_type='application/json')
+
+
+class WishlistView(View):
+   
+    def get(self, request):
+        # food = [
+        #     {'title': 'Bell Pepper',
+        #      'img': 'images/product-1.jpg',
+        #      'description': 'Far far away, behind the word mountains, far from the countries',
+        #      'currency': '$',
+        #      'price': 80.00,
+        #      'quantity': 1,
+        #      'total_price': 80.00
+        #      },
+        #     {'title': 'Stawberry',
+        #      'img': 'images/product-2.jpg',
+        #      'description': 'Far far away, behind the word mountains, far from the countries',
+        #      'currency': '$',
+        #      'price': 120.00,
+        #      'quantity': 1,
+        #      'total_price': 120.00
+        #      },
+        #     {'title': 'Green beans',
+        #      'img': 'images/product-3.jpg',
+        #      'description': 'Far far away, behind the word mountains, far from the countries',
+        #      'currency': '$',
+        #      'price': 20.00,
+        #      'quantity': 1,
+        #      'total_price': 20.00
+        #      },
+        # ]
+
+        product_query = Product.objects.all().values()
+        print(product_query)
+
+        food = list(product_query)
+
+        if not request.is_ajax():
+            return render(request, 'shop/wishlist.html', {'phone_number': PHONE_NUMBER})
+        else:
+            print("########")
+            return HttpResponse(json.dumps({'food': food}), content_type='application/json')
+
+        
+    # def post(self, request):
+    #     global wishlist_count
+    #     if request.is_ajax():
+    #         message = f'WishlistView: {wishlist_count}'
+    #         wishlist_count += 1
+    #         print(message)
+    #     else:
+    #         message = 'Not AJAX request'
+    #
+    #     return HttpResponse(message)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
